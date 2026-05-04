6670614840fba94362657c5b1fadc98be3014233
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-05-04 12:04:46 +0300

    Created the bottom navigation logic in the core

---
+++ vibe_project/lib/core/routing/routing_logic.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:vibe_project/features/home/presentation/screens/home_page.dart';
+import '../../features/home/presentation/screens/main_navigation.dart';
+// also import the chats the profile.. here
+
+class GenerateRoute{
+  static Route<dynamic> onGenerateRoute(RouteSettings settings){
+    final args = settings.arguments;
+
+    switch (settings.name){
+      case '/chat':
+
+      case '/profile':
+
+      case '/saved':
+
+      default:
+        // the default page wld be the homepage
+        return MaterialPageRoute(builder: (_) => MainNavigation());
+
+
+    }
+
+
+  }
+}
