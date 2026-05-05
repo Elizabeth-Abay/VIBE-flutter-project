55e528019ac7e0659f618edbec78d7fc94448444
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-05-05 14:15:39 +0300

    Done with the posting pages

--- vibe_project/lib/main.dart
+++ vibe_project/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './features/home/presentation/screens/main_navigation.dart';
+import './features/posts/presentation/screens/create_post.dart';
 
 
 void main() => runApp(MyApp());
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainNavigation(),
+      home: CreatePostPage(),
     );
   }
   
