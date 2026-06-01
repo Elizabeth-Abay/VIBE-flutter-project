8d9becbc76586fe5f537fc789eb81239a81e739f
Kidist Meseret <kidistmeseret1@gmail.com>
2026-06-02 01:08:37 +0900

    Add bcryptRelated

---
+++ backend/utils/bcryptRelated.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt');
+
+
+class BcryptRelated {
+    static async bcryptHasher(valueToBeHashed) {
+        try {
+            let saltgen = await bcrypt.genSalt();
+            let hashedVal = await bcrypt.hash(valueToBeHashed, saltgen);
+
+            return hashedVal;
+
+        } catch (err) {
+            err.from = 'BcryptRelated.bcrypt Hasher';
+
+            throw err;
+        }
+    }
+
+    static async bcryptCompare(unHashedValue, hashedValue) {
+        try {
+            // returns a boolean val
+           return await bcrypt.compare(unHashedValue, hashedValue);
+
+        } catch (err) {
+            err.from = 'BcryptRelated.bcrypt Hasher';
+
+            throw err;
+        }
+    }
+
+}
+
+
+
+module.exports = BcryptRelated;
