3d64f3f29bcd0926dbaa62d1f4d9aa504df6ad95
tsion kinde <tsionkinde275@gmail.com>
2026-06-01 07:35:29 -0700

    Adding ChatModel schema

---
+++ backend/model/schemas/ChatModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+    // id will be assigned automatically
+    participants: [
+        {
+            type: String, // <-- MongoDB ObjectId
+            ref: 'User',
+            required: true
+        }
+    ],
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    type: {
+        type: String,
+        enum: ['chat', 'self'], // Strictly limits the values to these two options
+        default: 'chat',        // Falls back to 'chat' if no type is provided
+        required: true          // Ensures the type field is always populated
+    }
+});
+
+
+
+const Chat = mongoose.model('Chat', ChatSchema);
+
+module.exports = Chat;
